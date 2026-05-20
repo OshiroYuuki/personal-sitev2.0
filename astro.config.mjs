bd77a03fd6d3a586c16ec74dee10f7b388c77df2
Yuuki <oshiro_yuki@outlook.jp>
2026-05-21 08:35:15 +0900

    making it even better hehehehehehhe

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: "https://oshiroyuuki.com",
+  base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: "Oshiro Yuuki",
+  EMAIL: "oshiroyuki@outlook.jp",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -10,7 +10,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Oshiro Yuuki's personal website and portfolio.",
 };
 
 export const BLOG: Metadata = {
@@ -35,10 +35,10 @@ export const SOCIALS: Socials = [
   },
   { 
     NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+    HREF: "https://github.com/oshiro-yuuki",
   },
   { 
     NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
+    HREF: "https://www.linkedin.com/in/悠希-大城-53999b27b",
   }
 ];
