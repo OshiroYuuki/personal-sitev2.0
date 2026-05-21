8ed60a37d461344205659afe5d182e5e8371e77e
Yuuki <oshiro_yuki@outlook.jp>
2026-05-21 09:10:29 +0900

    huh

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://www.oshiroyuuki.com",
+  site: "https://oshiroyuuki.com",
   base: "/",
   integrations: [mdx(), sitemap(), tailwind()],
 });
